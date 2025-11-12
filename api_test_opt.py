4d2a14e91625749ef00527847605baa4a6e62b1a
dlguswn3659 <dlguswn3659@naver.com>
2025-11-12 19:31:57 +0900

    [fix] best arch graph visualization & mobilenet_2bit work

--- api_test_opt.py
+++ api_test_opt.py
@@ -3,8 +3,8 @@ from ysautoml.optimization.fxp import train_fxp
 if __name__ == "__main__":
     train_fxp(
         # config="configs/resnet20_cifar100.yml",
-        config="configs/mobilenet_ori.yml",
-        # config="configs/mobilenet_2bit.yml",
+        # config="configs/mobilenet_ori.yml",
+        config="configs/mobilenet_2bit.yml",
         device="cuda:0",
         seed=42,
         # save_dir="./logs/fxp_cifar100",
